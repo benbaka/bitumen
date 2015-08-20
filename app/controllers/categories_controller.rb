c679d28a2cf1f7af5348a7352237c532b150c918
Benjamin Baka <baka@mpedigree.net>
2015-08-20 22:10:24 +0000

    Ben: method to handle creation of categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,7 +5,17 @@ class CategoriesController < ApplicationController
   end
 
   def create
+    @category = Category.create(category_params)
 
+    respond_to do |format|
+      if @category.save
+        format.html {redirect_to @category, notice: "Category was successfully created"}
+        format.json {render :show, status: :created, location: @category}
+      else
+        format.html {render :new}
+        format.json {render json: @category.errors, status: :unprocessable_entity}
+      end
+    end
   end
 
   def destroy
