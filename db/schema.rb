6ee576e834d312e6ff8cecbb3b170b5085781e1f
Benjamin Baka <baka@mpedigree.net>
2015-08-28 14:31:34 +0000

    Ben: schema changed for blog

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150820213523) do
+ActiveRecord::Schema.define(version: 20150828133411) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150820213523) do
     t.datetime "date_of_creation"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.integer  "category_id"
   end
 
   create_table "categories", force: :cascade do |t|
