f5434bc83b0e7de9f51a270eb17675835aba61fe
Benjamin Baka <baka@mpedigree.net>
2015-08-28 14:33:14 +0000

    Ben: added migration to add column to blog

---
+++ db/migrate/20150828133411_add_category_id_to_blog.rb
@@ -0,0 +1,10 @@
+class AddCategoryIdToBlog < ActiveRecord::Migration
+  def self.up
+    add_column :blogs, :category_id, :integer
+  end
+
+
+  def self.down
+    remove_column :blogs, category_id
+  end
+end
