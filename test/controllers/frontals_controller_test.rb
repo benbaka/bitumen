2f1548bb436939dcac20ad5d252778510abea5ef
Benjamin Baka <baka@mpedigree.net>
2015-08-28 23:12:36 +0000

    Ben: Added a test login user

--- test/controllers/frontals_controller_test.rb
+++ test/controllers/frontals_controller_test.rb
@@ -1,6 +1,18 @@
 require 'test_helper'
 
 class FrontalsControllerTest < ActionController::TestCase
+
+
+  test "should_login" do
+    @user = User.new
+    @user.username="ben"
+    @user.password="pass"
+    @user.save
+
+    post :login, frontal: {username: 'ben', password: 'password'}
+    assert_redirected_to users_path
+  end
+
   test "should get landing_page" do
     get :landing_page
     assert_response :success
