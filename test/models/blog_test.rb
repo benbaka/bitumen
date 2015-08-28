6cd9c83ed8669a794497c1ce762e60d8c2dd9908
Benjamin Baka <baka@mpedigree.net>
2015-08-28 14:32:02 +0000

    Ben: testing relationship between blogs and categories

--- test/models/blog_test.rb
+++ test/models/blog_test.rb
@@ -4,4 +4,17 @@ class BlogTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "a blog should belong to a category" do
+
+    category = Category.new({:name => "general"})
+    category.save
+
+    blog = Blog.new({:title => "My first blog", :summary => "My first blog"})
+    blog.body = "This is the first of a series of investigative blogs"
+    blog.category = category
+    blog.save
+
+    assert_equal blog.category.present?, true
+  end
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -4,4 +4,18 @@ class CategoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should add a blog to category" do
+
+    blog = Blog.new({:title => "My first blog", :summary => "My first blog"})
+    blog.body = "This is the first of a series of investigative blogs"
+    blog.save
+    category = Category.new({:name => "general"})
+    category.save
+
+    category.blogs << blog
+
+    assert_equal category.blogs.count, 1
+    assert_equal category.blogs.many?, false
+  end
 end
