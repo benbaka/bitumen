f96d133b5e12a4d4266f6eda53df614d639e5bf6
Benjamin Baka <baka@mpedigree.net>
2015-09-04 17:28:04 +0000

    Ben: Schema change due to ratings model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150828133411) do
+ActiveRecord::Schema.define(version: 20150904162804) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20150828133411) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.integer  "blog_id"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
