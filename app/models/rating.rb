1c269c39d31e6b596f6d887fd96d675d8e4138c6
Benjamin Baka <baka@mpedigree.net>
2015-09-05 03:19:54 +0000

    Ben: Added custom validation to prevent the same user from rating a blog twice

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,17 @@
 class Rating < ActiveRecord::Base
   belongs_to :blog
   belongs_to :user
+
+  validate :check_if_user_has_already_rated_blog
+
+  def check_if_user_has_already_rated_blog
+    user = User.find(self.user.id)
+
+    if self.blog != nil
+      if self.blog.ratings.find_by_user_id(self.user.id) != nil
+
+        errors.add(:user_id, "User has already rated blog")
+      end
+    end
+  end
 end
