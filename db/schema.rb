db02d4f27437066586673681f81357c54654e35a
Benjamin Baka <baka@mpedigree.net>
2015-09-06 21:54:26 +0000

    Ben: Added user_id to blog

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150904162804) do
+ActiveRecord::Schema.define(version: 20150906215234) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150904162804) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.integer  "category_id"
+    t.integer  "user_id"
   end
 
   create_table "categories", force: :cascade do |t|
