7fbfcf2d6c71ffb3441b8ff622e669f16e5ab958
Benjamin Baka <baka@mpedigree.net>
2015-09-07 08:32:38 +0000

    Ben: Added value parameter to permit method

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -69,6 +69,7 @@ class RatingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def rating_params
-      params.require(:rating).permit(:blog_id, :user_id)
+      params.require(:rating).permit(:blog_id, :user_id, :value)
     end
-end
+  end
+
