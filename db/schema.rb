87f4e039a931d6b8d6012509a9f58708df559311
Benjamin Baka <baka@mpedigree.net>
2015-09-12 08:11:34 +0000

    Ben: Added blog_id to comment with subsequent change in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150909230932) do
+ActiveRecord::Schema.define(version: 20150912075928) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20150909230932) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "blog_id"
   end
 
   create_table "ratings", force: :cascade do |t|
