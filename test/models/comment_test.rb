e65841b86a9b9ed2e2a878dd4387410ba5f7fdd3
Benjamin Baka <baka@mpedigree.net>
2015-09-12 09:04:29 +0000

    Ben: test blog and comment association and retrieval of all comments passed by a user

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -4,4 +4,24 @@ class CommentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should create a blog and assign comments to it" do
+    blog = blogs(:one)
+    user = users(:one)
+    comment1 = comments(:one)
+    comment2 = comments(:two)
+
+    comment1.user = user
+    comment2.user = user
+    comment1.save
+    comment2.save
+
+    blog.comments << comment1
+    blog.comments << comment2
+    blog.save
+
+
+    assert_not_empty blog.comments
+    assert_equal 2, Comment.where(:user_id => user.id).size
+  end
 end
