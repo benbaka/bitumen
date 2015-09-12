5050fe8df0465b6262a463f22d27ee4b2b3e371f
Benjamin Baka <baka@mpedigree.net>
2015-09-12 07:44:54 +0000

    Ben: A blog and user have many comments

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,6 +3,7 @@ class Blog < ActiveRecord::Base
   belongs_to :category
   has_many :ratings
   belongs_to :user
+  has_many :comments
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   has_many :ratings
   has_many :blogs
+  has_many :comments
 end
