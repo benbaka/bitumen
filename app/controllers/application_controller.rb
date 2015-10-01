19460e2732f1eb34da7772836f823344b17bbb33
Benjamin Baka <baka@mpedigree.net>
2015-10-01 12:11:32 +0000

    Ben: stop ajax calls from displaying flash messages twice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,26 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
+
   include SessionsHelper
 
+  after_filter :flash_headers
+
+  def flash_headers
+    # This will discontinue execution if Rails detects that the request is not
+    # from an AJAX request, i.e. the header wont be added for normal requests
+    return unless request.xhr?
+
+    # Add the appropriate flash messages to the header, add or remove as
+    # needed, but I think you'll get the point
+    response.headers['x-flash'] = flash[:error]  unless flash[:error].blank?
+    response.headers['x-flash'] = flash[:notice]  unless flash[:notice].blank?
+    response.headers['x-flash'] = flash[:warning]  unless flash[:warning].blank?
+
+    # Stops the flash appearing when you next refresh the page
+    flash.discard
+  end
 
 
   private
