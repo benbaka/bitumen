928ab1fb3904279592548cdabd379889505caec0
Benjamin Baka <baka@mpedigree.net>
2016-02-20 13:42:20 +0000

    Ben: added autocomplete url

--- config/routes.rb
+++ config/routes.rb
@@ -36,9 +36,13 @@ Rails.application.routes.draw do
 
   resources :users
 
+  # autocomplete url
+  get 'blogs/search' => 'blogs#search'
+
   resources :blogs do
     resources :comments
   end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
