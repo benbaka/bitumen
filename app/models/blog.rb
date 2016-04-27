9ff262830b51ad431db9245f36d52c352bb1caff
Benjamin Baka <baka@mpedigree.net>
2016-04-27 21:25:18 +0000

    Ben: paperclip specific attributes

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -6,5 +6,8 @@ class Blog < ActiveRecord::Base
   belongs_to :user
   has_many :comments
 
+  has_attached_file :blog_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :blog_image, content_type: /\Aimage\/.*\Z/
+
 
 end
