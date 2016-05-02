e3f8beaa9eb617dd60188933cf37d7f0b63e10bd
Benjamin Baka <baka@mpedigree.net>
2016-05-02 22:06:26 +0000

    Ben: using a manually created user, login and test to be redirected to the blogs page

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -4,6 +4,14 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @admin = User.new
+    @admin.username = "batman"
+    @admin.password = "batman"
+    @admin.save
+  end
+  
   test "should redirect to login when not logged in" do
     get "/blogs"
     assert_redirected_to frontals_home_url
@@ -12,6 +20,9 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
 
   test "should login and be redirected to blogs index" do
 
+    post "/frontals/login", :frontal=>{ username: "batman", password: "batman"}
+    assert_redirected_to blogs_path
+    assert_response 302
   end
 
 end
