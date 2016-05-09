e8c673053adf8f2656db67fcaabdc0a1a0147543
Benjamin Baka <baka@mpedigree.net>
2016-05-09 21:11:13 +0000

    Ben: renamed test and changed passing criteria

--- test/controllers/ratings_controller_test.rb
+++ test/controllers/ratings_controller_test.rb
@@ -1,8 +1,26 @@
 require 'test_helper'
+require 'mocha/setup'
+require 'mocha/test_unit'
+
 
 class RatingsControllerTest < ActionController::TestCase
   setup do
+    @blog = blogs(:one)
+    @admin = User.new
+    @admin.username = "batman"
+    @admin.password = "batman"
+    @admin.save
+    @blog.user = @admin
+    @blog.save
+
+    #stub out the current_user which is used to determine if one is logged in
+    ApplicationController.any_instance.stubs(:current_user).returns(@admin)
+
     @rating = ratings(:one)
+    @rating.user = @admin
+    @blog.ratings<<@rating
+    @rating.blog = @blog
+    @rating.save
   end
 
   test "should get index" do
@@ -16,12 +34,13 @@ class RatingsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create rating" do
-    assert_difference('Rating.count') do
+  test "should not create rating for same user twice for a blog" do
+    assert_no_difference('Rating.count') do
       post :create, rating: { blog_id: @rating.blog_id, user_id: @rating.user_id }
     end
 
-    assert_redirected_to rating_path(assigns(:rating))
+    assert_template :new
+    
   end
 
   test "should show rating" do
@@ -36,7 +55,7 @@ class RatingsControllerTest < ActionController::TestCase
 
   test "should update rating" do
     patch :update, id: @rating, rating: { blog_id: @rating.blog_id, user_id: @rating.user_id }
-    assert_redirected_to rating_path(assigns(:rating))
+    assert_redirected_to blog_path(@blog)
   end
 
   test "should destroy rating" do
